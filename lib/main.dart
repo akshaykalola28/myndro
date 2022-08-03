aa9268e374eecf35328520ddf00ed692503cb69a
Akshay Kalola <akshay.kalola28@gmail.com>
2022-08-03 16:11:01 +0530

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: EnterPhoneNumber.pageId,
+      initialRoute: SplashScreen.pageId,
       getPages: appPages,
     );
   }
