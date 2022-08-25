b0412cc47ad62dd5ba4564557d61380eb94941ce
Akshay Kalola <akshay.kalola28@gmail.com>
2022-08-25 17:11:35 +0530

    EXPERT_HOME

---
+++ lib/controller/expert_home_controller.dart
@@ -0,0 +1,7 @@
+import 'controller.dart';
+
+class ExpertDashboardController extends BaseController {
+  @override
+  void errorHandler(e) {}
+
+}
---
+++ lib/screens/expert_home/expert_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../controller/controller.dart';
+
+class ExpertHome extends GetView<ExpertDashboardController> {
+  static const pageId = "/ExpertDashboard";
+
+  const ExpertHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+body: ,
+    );
+  }
+}
